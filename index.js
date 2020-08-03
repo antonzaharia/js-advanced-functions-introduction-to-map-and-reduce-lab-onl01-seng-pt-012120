af46ccee324a5bf4ea559c409f78e561a7c34b0a
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-03 16:59:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 const mapToNegativize = function(arr) {
   let toReturn = [];
-  for( element of arr ) {
+  for( let element of arr ) {
     toReturn.push(element * -1);
   }
   return toReturn;
